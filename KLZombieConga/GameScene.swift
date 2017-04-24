3dfb2e27a93ec379befe87c8ec6436668cc0e83c
leon <leon@thentcircle.com>
2017-04-24 15:36:15 +0800

    Created

---
+++ KLZombieConga/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  KLZombieConga
+//
+//  Created by 康梁 on 2017/4/24.
+//  Copyright © 2017年 LeonKang. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        backgroundColor = SKColor.black
+    }
+}
---
+++ KLZombieConga/GameViewController.swift
@@ -0,0 +1,49 @@
+//
+//  GameViewController.swift
+//  KLZombieConga
+//
+//  Created by 康梁 on 2017/4/24.
+//  Copyright © 2017年 LeonKang. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene(size:CGSize(width: 2048, height: 1536))
+        let skView = self.view as! SKView
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        
+        scene.scaleMode = .aspectFill
+        
+        skView.presentScene(scene)
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
